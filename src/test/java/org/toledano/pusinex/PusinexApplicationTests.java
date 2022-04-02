631ca9709a5a2a40981c51164c1ed533a6e189ae
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-02 09:49:48 -0600

    Originación

---
+++ src/test/java/org/toledano/pusinex/PusinexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toledano.pusinex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PusinexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
