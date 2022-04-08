816cc094f66ed452a54802adb8c76c95bfed51ac
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-08 14:05:58 -0500

    Mejora en la consulta.
    Usando ahora elementos transversales.

--- src/main/java/org/toledano/pusinex/models/repository/IDistritoFederalRepository.java
+++ src/main/java/org/toledano/pusinex/models/repository/IDistritoFederalRepository.java
@@ -8,8 +8,7 @@ import org.toledano.pusinex.models.entity.DistritoFederal;
 import java.util.List;
 
 public interface IDistritoFederalRepository extends CrudRepository<DistritoFederal, Integer> {
-    @Query("SELECT d FROM DistritoFederal d WHERE d.entidadId.entidad=:entidadId")
-    List<DistritoFederal> findByEntidadId(@RequestParam("entidadId") int entidadId);
+    List<DistritoFederal> findByEntidadId_entidad(int entidad);
     @Query("SELECT d FROM DistritoFederal d WHERE d.entidadId.entidad=:entidadId AND d.distritoFederal=:distrito")
     DistritoFederal findByEntidadIdAndDistrito(@RequestParam("entidadId") int entidadId, @RequestParam("distrito") int distrito);
 }
