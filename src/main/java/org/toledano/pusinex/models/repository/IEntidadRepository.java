691f6c84b9da90a50bc5627599d7df1066fc5a47
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-08 10:00:27 -0500

    Se corrige el nombre de la interface de entidad

--- src/main/java/org/toledano/pusinex/models/repository/IEntidadRepository.java
+++ src/main/java/org/toledano/pusinex/models/repository/IEntidadRepository.java
@@ -3,8 +3,6 @@ package org.toledano.pusinex.models.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.toledano.pusinex.models.entity.Entidad;
 
-import java.util.List;
-
-public interface EntidadRepository extends CrudRepository<Entidad, Integer> {
+public interface IEntidadRepository extends CrudRepository<Entidad, Integer> {
     Entidad findByEntidad(int entidad);
 }
