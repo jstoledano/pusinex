288c279d0d2ff86e203b9c20a46d9c5a7c940f6a
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-12 12:40:21 -0500

    Update StatusPusinex.java

--- src/main/java/org/toledano/pusinex/models/entity/StatusPusinex.java
+++ src/main/java/org/toledano/pusinex/models/entity/StatusPusinex.java
@@ -6,16 +6,13 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Setter
 @Getter
-@Table(name = "app_statuspusinex")
 public class StatusPusinex {
-    @Id @Column(nullable = false,columnDefinition = "nvarchar", length = 1)
-    private String status;
+    @Id @Column(length = 1)
+    private String id;
 
-    @Column(nullable = false, columnDefinition = "nvarchar", length = 100)
     private String descripcion;
 }
