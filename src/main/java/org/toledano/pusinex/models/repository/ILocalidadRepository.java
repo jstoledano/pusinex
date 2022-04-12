7bbabe52f4ab5f43e97f73db22d35c77d3ba5683
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-12 12:39:53 -0500

    Localidad

--- src/main/java/org/toledano/pusinex/models/repository/ILocalidadRepository.java
+++ src/main/java/org/toledano/pusinex/models/repository/ILocalidadRepository.java
@@ -7,18 +7,18 @@ import org.toledano.pusinex.models.entity.Localidad;
 import java.util.List;
 
 public interface ILocalidadRepository extends CrudRepository<Localidad, Integer> {
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1 and l.seccionId.seccion = ?2 and l.localidad = ?3")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1 and l.seccion.seccion = ?2 and l.localidad = ?3")
     public Localidad findLocalidad(int entidad, int seccion, int localidad);
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1 and l.seccionId.municipioId.municipio = ?2")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1 and l.seccion.municipio.municipio = ?2")
     public List<Localidad> findLocalidadByMunicipio(int entidad, int municipio);
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1")
     public List<Localidad> findAll(int entidad);
 
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1 and l.seccionId.seccion = ?2 and l.localidad = ?3 and l.tipo = ?4")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1 and l.seccion.seccion = ?2 and l.localidad = ?3 and l.tipo = ?4")
     public Localidad findLocalidadAndTipo(int entidad, int seccion, int localidad, String tipo);
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1 and l.seccionId.municipioId.municipio = ?2 and l.tipo = ?3")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1 and l.seccion.municipio.municipio = ?2 and l.tipo = ?3")
     public List<Localidad> findLocalidadByMunicipioAndTipo(int entidad, int municipio, String tipo);
-    @Query("SELECT l FROM Localidad l WHERE l.seccionId.entidadId.entidad = ?1 and l.tipo = ?2")
+    @Query("SELECT l FROM Localidad l WHERE l.seccion.entidad.entidad = ?1 and l.tipo = ?2")
     public List<Localidad> findAllAndTipo(int entidad, String tipo);
 
 }
