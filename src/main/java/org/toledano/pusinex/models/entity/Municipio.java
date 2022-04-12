a04438a1949f7a8ef831d51f79bea2b42a40ed03
Javier Sanchez Toledano <jstoledano@users.noreply.github.com>
2022-04-12 12:40:03 -0500

    Municipio

--- src/main/java/org/toledano/pusinex/models/entity/Municipio.java
+++ src/main/java/org/toledano/pusinex/models/entity/Municipio.java
@@ -2,29 +2,25 @@ package org.toledano.pusinex.models.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 
 @Setter
 @Getter
 @Entity
-@Table(name="app_municipio", uniqueConstraints={
-        @UniqueConstraint( name = "idx_Entidad_Municipio",  columnNames ={"entidad_id","municipio"})
-})
+@Table(uniqueConstraints={@UniqueConstraint(columnNames ={"entidad_entidad","municipio"})})
 public class Municipio {
     @Id
     private Integer municipio;
 
-    @Column(name = "nombre", nullable = false, columnDefinition = "nvarchar", length = 100)
     private String nombre;
 
     @OneToOne
-    @JoinColumn(name="entidad_id", referencedColumnName = "entidad")
-    private Entidad entidadId;
+    @JoinColumn()
+    private Entidad entidad;
 
     @Override
     public  String toString() {
-        return String.format("%02d", this.entidadId.getEntidad()) +
+        return String.format("%02d", this.entidad.getEntidad()) +
                 String.format("%03d", this.municipio) + " " +
                 this.nombre.toUpperCase();
     }
