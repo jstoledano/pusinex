72712a0fe53d38883a2b6c1d0ed10637505421b9
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-03-24 11:21:33 -0600

    Funciona la descarga de PUSINEX.

--- control/urls.py
+++ control/urls.py
@@ -4,7 +4,7 @@ from control.views import (
     MunicipioViewSet, LocalidadViewSet, SeccionViewSet,  PusinexViewSet,
     Index, Administration, CreatePUSINEX,
     PusinexDetail, LocalidadDetail, MunicipioDetail,
-    MunicipioAutoComplete, VNM2023
+    MunicipioAutoComplete, VNM2023, VNMZipView
 )
 
 router = routers.SimpleRouter()
@@ -17,6 +17,7 @@ router.register(r'pusinex', PusinexViewSet)
 urlpatterns = [
     re_path(r'^municipio-autocomplete/$', MunicipioAutoComplete.as_view(), name='municipio-autocomplete'),
     path('vnm/', VNM2023.as_view(), name='vnm'),
+    path('vnm/<int:dto>', VNMZipView.as_view(), name='vnmZip'),
     path('pusinex/<int:pk>', PusinexDetail.as_view(), name='pusinex'),
     path('localidad/<int:pk>', LocalidadDetail.as_view(), name='localidad'),
     path('municipio/<int:pk>', MunicipioDetail.as_view(), name='municipio'),
--- control/views.py
+++ control/views.py
@@ -1,10 +1,14 @@
 from dal import autocomplete
 from django.db.models import Q
+from django.views import View
 from django.views.generic import TemplateView, DetailView, CreateView, ListView
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters.views import FilterView
 from django.urls import reverse, reverse_lazy
-from django.contrib.auth import logout
+import zipfile
+from django.http import HttpResponse, FileResponse
+import os
+from pathlib import Path
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import viewsets
 
@@ -121,11 +125,35 @@ class LogoutView(TemplateView):
     redirect_field_name = 'next'
 
 
+seccionesVNM2023 = (
+    12, 14, 16, 17, 26, 27, 30, 34, 36, 43, 48, 74, 107, 184, 188,
+    201, 203, 218, 261, 409, 414, 474, 478, 482, 506, 542, 543,
+    533, 534, 606, 9, 124, 134, 141, 147, 266, 348, 349, 355, 363,
+    364, 384, 624, 397, 441, 442, 469, 626, 567, 392, 159, 151, 2,
+    78, 85, 87, 89, 234, 242, 290, 297, 325, 336, 425, 511, 512,
+    515, 575, 581, 589, 593, 597, 314, 551, 473, 142)
+queryVNM2023 = Seccion.objects.filter(seccion__in=seccionesVNM2023).order_by('distrito', 'seccion')
+pusinexVNM2023 = Pusinex.objects.filter(seccion__seccion__in=seccionesVNM2023)
+
+
 class VNM2023(ListView):
-    secciones = (12, 14, 16, 17, 26, 27, 30, 34, 36, 43, 48, 74, 107, 184, 188, 201, 203, 218, 261, 409, 414, 474, 478, 482, 506, 542, 543, 533, 534, 606, 9, 124, 134, 141, 147, 266, 348, 349, 355, 363, 364, 384, 624, 397, 441, 442, 469, 626, 567, 392, 159, 151, 2, 78, 85, 87, 89, 234, 242, 290, 297, 325, 336, 425, 511, 512, 515, 575, 581, 589, 593, 597, 314, 551, 473, 142)
     model = Seccion
 
     def get_queryset(self):
-        qs = super().get_queryset()
-        qs = Seccion.objects.filter(seccion__in=self.secciones).order_by('distrito', 'seccion')
+        qs = queryVNM2023
         return qs
+
+
+class VNMZipView(View):
+    @staticmethod
+    def get(request, dto):
+        files = []
+        zip_name = Path('media', 'pusinex', f'pusinex_VNM_0{dto}.zip')
+        zip_archive = zipfile.ZipFile(zip_name, mode='w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)
+        if dto:
+            for p in pusinexVNM2023.filter(seccion__distrito__distrito=dto):
+                files.append(Path(os.getcwd(), 'media', p.revision_set.latest().archivo.path))
+        with zip_archive as archive:
+            for file in files:
+                archive.write(file, arcname=Path(file).name)
+        return FileResponse(open(zip_name, 'rb'))
