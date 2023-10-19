e7560c04feb74e06a6fa95e5eada861cc022c3f5
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-10-19 11:18:38 -0600

    Modelos BGD básicos. Caso #2

---
+++ models/bgd.go
@@ -0,0 +1,26 @@
+package pusinex
+
+import (
+	"gorm.io/gorm"
+)
+
+type Distrito struct {
+	gorm.Model
+	Distrito uint
+	Cabecera string
+}
+
+type Municipio struct {
+	gorm.Model
+	Municipio uint
+	Nombre    string
+}
+
+type Seccion struct {
+	gorm.Model
+	Sección   uint
+	Distrito  uint
+	Municipio uint
+	Tipo      uint
+	Estado    uint
+}
