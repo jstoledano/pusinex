a44eb699e40e6c15064acc92407858226354387c
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-11-06 16:22:04 -0600

    Ya funciona la carga con PUSINEX sin localidad.

--- control/forms.py
+++ control/forms.py
@@ -18,8 +18,12 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+
 class PUSINEXForm(forms.ModelForm):
-    seccion = forms.ModelChoiceField(queryset=Seccion.objects.all().order_by('seccion'))
+    seccion = forms.ModelChoiceField(
+        queryset=Seccion.objects.filter(activa=True).order_by('seccion'),
+        label='Sección',
+    )
     f_act = forms.DateField(label='Fecha de Actualización', widget=forms.DateInput(attrs={'type': 'date'}))
     hojas = forms.IntegerField(min_value=1)
     archivo = forms.FileField()
